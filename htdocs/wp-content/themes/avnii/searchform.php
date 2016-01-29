83db6e983e92d54c8e09d1562d0db76ba96aab1a
greg_cip <gregorypeigne1@gmail.com>
2016-01-29 16:02:21 +0100

    maj dossiers

--- htdocs/wp-content/themes/avnii/searchform.php
+++
@@ -1,8 +0,0 @@
-<form role="search" method="get" class="searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>">
-    <div>
-        <input type="text" placeholder="Search" name="s" id="search" />
-        <input type="submit" class="fa fa-search" id="searchsubmit" value="Search" />
-    </div>
-</form>
-<div class="clear"></div>
-<br/>
