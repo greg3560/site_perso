44ed572f651e86b7d796f902212b6da75a8c3bae
greg_cip <gregorypeigne1@gmail.com>
2016-02-10 16:15:22 +0100

    finalisation du menu et en-tete puis ajout de quelques articles pour tester les categories

--- htdocs/wp-content/themes/voice-child/functions.php
+++ htdocs/wp-content/themes/voice-child/functions.php
@@ -16,22 +16,5 @@ function vce_child_load_scripts() {
 	wp_enqueue_style('vce_child_load_scripts');
 }
 
-if ( function_exists('register_sidebar') ) {
-register_sidebar(array(
-        'name' => 'Sidebar',
-	'before_widget' => '<div class="widget_sidebar">',
-        'after_widget' => '</div>',
-        'before_title' => '<h3>',
-        'after_title' => '</h3>',
-      ));
-register_sidebar(array(
-        'name' => 'header_right',
-	'before_widget' => '<div class="header_right" id="%1$s">',
-        'after_widget' => '</div>',
-        'before_title' => '<h3>',
-        'after_title' => '</h3>',
-      ));	  
-	}
-
 
 ?>
