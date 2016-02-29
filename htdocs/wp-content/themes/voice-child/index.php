c1dafe36f766265b34683e5eb4366f971c33e4dd
greg_home <gregorypeigne1@gmail.com>
2016-02-29 17:52:14 +0100

    fin de page d'accueil

--- htdocs/wp-content/themes/voice-child/index.php
+++ htdocs/wp-content/themes/voice-child/index.php
@@ -11,6 +11,7 @@ get_header(); ?>
 	<?php global $vce_sidebar_opts; ?>
 
 	<?php if ( $vce_sidebar_opts['use_sidebar'] == 'left' ) { get_sidebar(); } ?>
+
 		
 	<div id="primary" class="vce-main-content">
 <?php echo do_shortcode("[huge_it_videogallery id='1']");
@@ -60,13 +61,41 @@ get_header(); ?>
 			</div>
 
 		</div>
-	
+		<div class="main-box">
+
+			<?php get_template_part('../voice-child/archive-title'); ?>
+
+			<div class="main-box-inside">
+			
+			<?php if ( have_posts() ) : ?>
+				
+				<div class="vce-loop-wrap">
+				<?php query_posts('category_name=alaune'); ?>
+				<?php while ( have_posts() ) : the_post(); ?>
+					<?php get_template_part( 'sections/loops/layout-'.vce_get_archive_layout()); ?>
+				<?php endwhile; ?>
+				</div>
+				<?php get_template_part( 'sections/pagination/'.vce_get_archive_pagination()); ?>
+
+			<?php else: ?>
+				
+				<?php get_template_part( 'sections/content-none'); ?>
+
+			<?php endif; ?>
+			
+
+			</div>
+
+		</div>
 
 	</div>
 
 	<?php if ( $vce_sidebar_opts['use_sidebar'] == 'right' ) { get_sidebar(); } ?>
 
 
-</div>
 
+		
+
+
+</div>
 <?php get_footer(); ?>
