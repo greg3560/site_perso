fb11f7c0c3e18fd241f88f6492a8c44afddd5ba4
greg_windows-home <gregorypeigne1@gmail.com>
2016-09-23 11:07:28 +0200

    optimisation des css 2

--- htdocs/index.php
+++ htdocs/index.php
@@ -25,17 +25,12 @@
 		<meta itemprop="image" content="http://www.gregorypeigne.fr/assets/img/photo_gregory.png">
 		<title>Grégory Peigné</title>
 		<meta name="description" content="Recherche contrat pro ou CDD/CDI développeur web.">
-        <!-- CDN CSS materialize compilé et minifié -->
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
 		<!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
 		<!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
 		<!--[if lt IE 9]>
 			<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
 			<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 		<![endif]-->
-		<!-- CSS perso -->
-		<!-- <link href='css/style.css' rel='stylesheet' type='text/css'> -->
-		<link href='css/pre_style.min.css' rel='stylesheet' type='text/css'>
         <!-- Une petite favicon -->
         <link rel="icon" type="image/png" href="favicon.png" />
         <!--[if IE]><link rel="shortcut icon" type="image/x-icon" href="favicon.ico" /><![endif]-->
@@ -999,14 +994,18 @@
             	</div>
           	</div>
 		</footer>
-        <!--Import Google Icon Font-->
-        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
-        <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
+        <!-- CDN CSS materialize compilé et minifié -->
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.97.7/css/materialize.min.css">
         <!-- CSS perso -->
+        <!-- <link href='css/pre_style.css' rel='stylesheet' type='text/css'> -->
+        <link href='css/pre_style.min.css' rel='stylesheet' type='text/css'>
         <!-- <link href='css/style.css' rel='stylesheet' type='text/css'> -->
         <link href='css/style.min.css' rel='stylesheet' type='text/css'>
         <!-- <link href='css/owl.carousel.css' rel='stylesheet' type='text/css'> -->
         <link href='css/owl.carousel.min.css' rel='stylesheet' type='text/css'>
+        <!--Import Google Icon Font-->
+        <link href="http://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css?family=Kaushan+Script" rel="stylesheet">
 		<!-- script partage sur les reseaux sociaux -->
 		<script type="text/javascript" src="http://platform.linkedin.com/in.js"></script>
 		<script type="text/javascript" src="http://platform.twitter.com/widgets.js"></script>
