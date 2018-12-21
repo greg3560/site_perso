054353550dadb89ee58d726e3043c33e976271a7
greg3560 <gregorypeigne1@gmail.com>
2018-12-21 18:21:08 +0100

    configuration gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 'use strict';
 
-var gulp = require('gulp'),
+let gulp = require('gulp'),
     sass = require('gulp-sass'),
     livereload = require('gulp-livereload'),
     useref = require('gulp-useref'),
@@ -12,9 +12,26 @@ var gulp = require('gulp'),
     zip = require('gulp-zip'),
     twig = require('gulp-twig'),
     webpack = require('webpack-stream'),
-    content = require('./js/content');
+    content = require('./js/content'),
+    git = require('gulp-git');
 
 sass.compiler = require('node-sass');
+
+gulp.task('webpack', function () {
+    return gulp.src('js/*.js')
+        .pipe(webpack({
+            output: {
+                filename: 'main.js',
+            },
+        }))
+        .pipe(gulp.dest('output/'));
+});
+
+gulp.task('sass', function () {
+    return gulp.src('./sass/materialize.scss')
+        .pipe(sass().on('error', sass.logError))
+        .pipe(gulp.dest('./css'));
+});
 
 gulp.task('twig', function () {
     return gulp.src('./templates/index.twig')
@@ -24,23 +41,14 @@ gulp.task('twig', function () {
         .pipe(gulp.dest('./'));
 });
 
-gulp.task('babel', function() {
-    return gulp.src('js/*.js')
-        .pipe(webpack())
-        .pipe(gulp.dest('output/'));
-});
+gulp.task('default', ['webpack', 'sass', 'twig'], () => {
 
-
-gulp.task('sass', function () {
-    return gulp.src('./sass/materialize.scss')
-        .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('./css'));
 });
 
 gulp.task('clean', function () {
     gulp.src('dist', {read: false})
         .pipe(clean());
-    return gulp.src('output', {read: false})
+    gulp.src('output', {read: false})
         .pipe(clean());
 });
 
@@ -50,24 +58,37 @@ gulp.task('img', ['clean'], function () {
         .pipe(gulp.dest('dist/img'));
 });
 
-gulp.task('dist', ['babel', 'sass', 'img', 'twig'], function() {
-    return gulp.src('*.html')
+gulp.task("fonts", function() {
+    return gulp.src("fonts/**/*.*")
+        .pipe(gulp.dest('dist/fonts'));
+});
+
+gulp.task('dist', ['webpack', 'sass', 'twig', 'img', 'fonts'], function () {
+    gulp.src('*.html')
         .pipe(useref())
         .pipe(gulpif('*.js', uglify()))
         .pipe(gulpif('*.css', minifyCss()))
         .pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['dist'], function() {
+// Run git push
+// remote is the remote repo
+// branch is the remote branch to push to
+gulp.task('push', function(){
+    git.push('origin', 'master', function (err) {
+        if (err) throw err;
+    });
+});
+
+gulp.task('prod', ['dist', 'push'], () => {
     return gulp.src('dist/*')
         .pipe(zip('dist.zip'))
         .pipe(gulp.dest('.'));
 });
 
-gulp.task('watch', function () {
+gulp.task('watch', () => {
     livereload.listen();
-    gulp.watch('sass/*scss', ['sass']);
-    gulp.watch(['*.html', 'templates/*.twig', '/sass/*.scss', 'js/*.js', 'css/style.css']).on('change', function(e) {
+    gulp.watch(['js/*.js', 'sass/*scss', 'templates/*.twig'], ['webpack', 'sass', 'twig']).on('change', function (e) {
         livereload.changed(e.path);
     });
 });
